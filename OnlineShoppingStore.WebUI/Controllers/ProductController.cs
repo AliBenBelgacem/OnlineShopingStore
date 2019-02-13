b40fa7cabc9c35503316ce8ebf5526a6a01b2e0a
Ben Belgacem Ali <BBA@tgw-group.com>
2019-02-13 14:11:51 +0100

    Part 5

--- OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,21 @@ namespace OnlineShoppingStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository _repository;
+
+        private readonly int pagesize=3;
         public ProductController(IProductRepository repo)
         {
             _repository = repo;
         }
         // GET: Product
-        public ActionResult List()
+        public ActionResult List(int page = 1)
         {
-            return View(_repository.Products);
+            ProductsListViewModel plVM = new ProductsListViewModel()
+            {
+                Products = _repository.Products.OrderBy(p => p.ProductId).Skip(page-1* pagesize).Take(pagesize),
+                PagingInfo = new PagingInfo() { CurrentPage = page, ItemsPerPage = 3, TotalItems = _repository.Products.Count() }
+            };
+            return View(plVM);
         }
     }
 }
